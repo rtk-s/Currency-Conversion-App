de21cd7d7ccd51efcb5bfd52860b7407d55957ae
rtk-s <nikolay.malofeev@gmail.com>
2015-10-15 20:29:12 +0300

    Conversation app

---
+++ Quiz/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Nikolay on 15.10.15.
+//  Copyright © 2015 nut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Quiz/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Quiz
+//
+//  Created by Nikolay on 15.10.15.
+//  Copyright © 2015 nut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
